e86eff2a679b639ba18fc2f7d0e1d77cdab70f0e
EddieNg1 <eddieng.195@gmail.com>
2023-02-12 21:46:58 -0500

    Add getById route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -30,5 +30,5 @@ router.get('/fragments', require('./get'));
 // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
 // You can use Buffer.isBuffer(req.body) to test if it was parsed by the raw body parser.
 router.post('/fragments', rawBody(), require('./post'));
-
+router.get('/fragments/:id', require('./getById'));
 module.exports = router;
---
+++ tests/unit/getById.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+describe('GET /v1/fragments:id', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/id').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/id')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+});
