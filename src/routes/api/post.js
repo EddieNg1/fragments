e7a0036afe1a84d4ac2aeca90f7ff2042e477afc
EddieNg1 <eddieng.195@gmail.com>
2023-02-12 20:59:33 -0500

    Update POST test

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -4,20 +4,30 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
-  const api = process.env.API_URL || req.headers.host;
+  //const api = process.env.API_URL || req.headers.host;
 
   const data = req.body;
-  const user = req.user;
+  //const user = req.user;
   const contentType = req.headers['content-type'];
   logger.debug(`Fragment content type is ${contentType}`);
   try {
-    const fragment = new Fragment({ ownerId: user, type: contentType });
+    // const fragment = new Fragment({ ownerId: user, type: contentType });
+    // await fragment.save();
+    // await fragment.setData(data);
+    // res.setHeader('Content-type', fragment.type);
+    // res.setHeader('Location', `${api}/v1/fragments/${fragment.id}`);
+    // res.status(200).json(createSuccessResponse({ fragment }));
+    // logger.info('Successfully created fragment');
+    const fragment = new Fragment({
+      ownerId: req.user,
+      type: contentType,
+    });
     await fragment.save();
     await fragment.setData(data);
-    res.setHeader('Content-type', fragment.type);
-    res.setHeader('Location', `${api}/v1/fragments/${fragment.id}`);
-    res.status(200).json(createSuccessResponse({ fragment }));
-    logger.info('Successfully created fragment');
+
+    logger.debug({ fragment }, 'Fragment created successfully');
+    res.setHeader('Location', `${process.env.API_URL}/v1/fragments/${fragment.id}`);
+    res.status(201).json(createSuccessResponse({ fragment }));
   } catch (err) {
     logger.warn(err.message, 'Failed to create fragment');
     res.status(500).json(createErrorResponse(500, err.message));
