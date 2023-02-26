9857321cab6f3a1b3f70b7d05861f8431e3d0d59
Eddie Ng <eng42@myseneca.ca>
2023-02-26 13:03:50 -0500

    Test get.js

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -9,13 +9,6 @@ module.exports = async (req, res) => {
   // TODO: this is just a placeholder to get something working...
   var expand = false;
   try {
-    if (req.query.expand !== '1') {
-      const error = new Error('Invalid expand query');
-      error.status = 400;
-      throw error;
-    } else {
-      expand = true;
-    }
     const fragments = await Fragment.byUser(req.user, expand);
     res.status(200).json(createSuccessResponse({ fragments }));
   } catch (error) {
