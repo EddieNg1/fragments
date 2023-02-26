3b51ac17dc37da0acf12fdd2428b6c92f85f0a8f
Eddie Ng <eng42@myseneca.ca>
2023-02-26 14:16:29 -0500

    Test

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -9,15 +9,18 @@ module.exports = async (req, res) => {
   // TODO: this is just a placeholder to get something working...
   var expand = false;
   try {
-    if (
-      Object.keys(req.query).length > 0 &&
-      (!req.query.expand.length || req.query.expand !== '1')
-    ) {
-      const error = new Error('Invalid expand query');
-      error.status = 400;
-      throw error;
-    } else {
-      expand = true;
+    if (Object.keys(req.query).length > 0) {
+      if (req.query.expand.length === 0) {
+        const error = new Error('Missing expand value');
+        error.status = 400;
+        throw error;
+      } else if (req.query.expand !== '1') {
+        const error = new Error('Invalid expand value');
+        error.status = 400;
+        throw error;
+      } else {
+        expand = true;
+      }
     }
     const fragments = await Fragment.byUser(req.user, expand);
     res.status(200).json(createSuccessResponse({ fragments }));
