94faa97116f3d8756e7ab0cffc6cef3c472c1d4b
Eddie Ng <eng42@myseneca.ca>
2023-02-26 12:50:49 -0500

    Fix get.js

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -9,7 +9,7 @@ const { createSuccessResponse } = require('../../response');
 module.exports = async (req, res) => {
   var expand = false;
   try {
-    if (!req.query.expand.length == 0 || req.query.expand != '1') {
+    if (req.query.expand !== '1') {
       const error = new Error('Invalid expand query');
       error.status = 400;
       throw error;
@@ -17,7 +17,6 @@ module.exports = async (req, res) => {
       expand = true;
     }
     const fragments = await Fragment.byUser(req.user, expand);
-    res.setHeader('Location', `${process.env.API_URL}/v1/fragments/${fragments.id}`);
     res.status(200).json(createSuccessResponse({ fragments }));
   } catch (error) {
     throw new Error('Error getting fragments for current user');
