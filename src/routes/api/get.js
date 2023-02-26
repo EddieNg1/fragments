13f5df8fadadabe57bfc871e1c2e035918b5d272
Eddie Ng <eng42@myseneca.ca>
2023-02-26 12:56:55 -0500

    Fix get.js

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,12 +1,12 @@
 // src/routes/api/get.js
 
-const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse } = require('../../response');
-
+const { Fragment } = require('../../model/fragment');
 /**
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
+  // TODO: this is just a placeholder to get something working...
   var expand = false;
   try {
     if (req.query.expand !== '1') {
