73aa1377a616aa9fa0604d5b727cbfd6a712d8a6
Eddie Ng <eng42@myseneca.ca>
2023-04-08 02:38:25 -0400

    git update-index --chmod=+x .scripts/local-aws-setup.sh

--- scripts/local-aws-setup.sh
+++ scripts/local-aws-setup.sh
@@ -30,7 +30,7 @@ echo 'LocalStack S3 Ready'
 echo "Creating LocalStack S3 bucket: fragments"
 aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket fragments
 
-# Setup DynamoDB Table with dynamodb-local, see: 
+# Setup DynamoDB Table with dynamodb-local, see:
 # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/getting-started-step-1.html
 echo "Creating DynamoDB-Local DynamoDB table: fragments"
 aws --endpoint-url=http://localhost:8000 \
