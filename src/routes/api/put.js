dbcfecdbf9c4563bfcde0154b1ad3fc067b2a83f
Eddie Ng <eng42@myseneca.ca>
2023-04-16 11:29:47 -0400

    Update put

--- src/routes/api/put.js
+++ src/routes/api/put.js
@@ -1,20 +1,19 @@
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 module.exports = async (req, res) => {
+  const api = process.env.API_URL || req.headers.host;
   try {
-    const fragment = await Fragment.byId(req.user, req.params.id);
-    if (fragment.type == req.get('Content-Type')) {
-      await fragment.setData(req.body);
-      res.status(200).json(
-        createSuccessResponse({
-          status: 'ok',
-          fragment: fragment,
-        })
-      );
-    } else {
-      res.status(400).json(createErrorResponse(400, 'Cannot change fragment type'));
-    }
-  } catch (error) {
-    res.status(404).json(createErrorResponse(404, error));
+    let id = req.params.id;
+    const fragmentContent = req.body;
+    const fragment = await Fragment.byId(req.user, id);
+    await fragment.setData(fragmentContent);
+    await fragment.save();
+    res
+      .status(200)
+      .location(`${api}/v1/fragments/${fragment.id}`)
+      .json(createSuccessResponse(fragment));
+  } catch (err) {
+    console.log(err);
+    res.status(404).json(createErrorResponse(404, err));
   }
 };
